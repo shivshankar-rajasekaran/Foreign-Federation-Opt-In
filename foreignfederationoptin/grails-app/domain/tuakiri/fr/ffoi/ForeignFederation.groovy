c163cdf9ed3806fe6f2131c9615eca815e55cc18
shivshankar rajasekaran <sraj786@aucklanduni.ac.nz>
2012-01-10 16:57:46 +1300

    new plugin

---
+++ foreignfederationoptin/grails-app/domain/tuakiri/fr/ffoi/ForeignFederation.groovy
@@ -0,0 +1,35 @@
+package tuakiri.fr.ffoi
+
+class ForeignFederation {
+    String name
+    String displayName
+    String description
+//    URlURI websiteURL
+//    URlURI metadataURL
+    String metadataName
+
+    //static hasMany = [contacts: ContactPerson]
+
+    static constraints = {
+                              name(nullable: false,blank:false,unique :true)
+
+                              displayName(nullable: false, blank:false)
+
+                              description(nullable: true, blank:true)
+
+                              contacts(nullable: true)
+
+                              extensions(nullable: true)
+
+                              registrationInfo(nullable:false)
+
+                              dateCreated(nullable:false,blank:false)
+
+                              dateUpdated(nullable:false,blank:false)
+
+                              websiteURL(nullable:false)
+
+                              metaDataURL(nullable:false)
+    }
+
+}
---
+++ foreignfederationoptin/test/unit/foreignfederationoptin/TuakiriTests.groovy
@@ -0,0 +1,17 @@
+package foreignfederationoptin
+
+import grails.test.*
+
+class TuakiriTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}

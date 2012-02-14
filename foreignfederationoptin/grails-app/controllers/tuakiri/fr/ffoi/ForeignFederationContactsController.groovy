bc094f125f066941c8d75b1d1b426d27d5295949
A.R Chopra <acho050@aucklanduni.ac.nz>
2012-02-14 19:01:35 +1300

    foreign fed 14 feb update

---
+++ foreignfederationoptin/grails-app/controllers/tuakiri/fr/ffoi/ForeignFederationContactsController.groovy
@@ -0,0 +1,131 @@
+package tuakiri.fr.ffoi
+
+import org.apache.shiro.SecurityUtils
+import fedreg.core.*
+
+class ForeignFederationContactsController {
+
+   def allowedMethods = [create:'POST', delete:'DELETE']
+
+	def search = {
+		def contacts
+
+		if(!params.givenName && !params.surname && !params.email)
+			contacts = Contact.list()
+		else {
+			def emails
+			if(params.email)
+				emails = MailURI.findAllByUriLike("%${params.email}%")
+			def c = Contact.createCriteria()
+			contacts = c.list {
+				or {
+					ilike("givenName", params.givenName)
+					ilike("surname", params.surname)
+					if(emails)
+						'in'("email", emails)
+				}
+			}
+		}
+		render template:"/templates/foreignFederationContacts/results", contextPath: pluginContextPath, model:[contacts:contacts]
+	}
+        def create = {
+		if(!params.id || !params.contactID || !params.contactType) {
+			log.warn "All name/value pairs required for this call were not present"
+			render message(code: 'fedreg.controllers.namevalue.missing')
+			response.setStatus(500)
+			return
+		}
+
+		def foreignFederation = ForeignFederation.get(params.id)
+		if(!foreignFederation) {
+			log.warn "RoleDescriptor identified by id $params.id was not located"
+			render message(code: 'fedreg.roledescriptor.nonexistant', args: [params.id])
+			response.setStatus(500)
+			return
+		}
+
+		if(SecurityUtils.subject.isPermitted("foreignFederation:${foreignFederation.id}:contact:add")) {
+			def contact = Contact.get(params.contactID)
+			if(!contact) {
+				log.warn "Contact identified by id $params.contactID was not located"
+				render message(code: 'fedreg.contact.nonexistant', args: [params.contactID])
+				response.setStatus(500)
+				return
+			}
+
+			def contactType = ContactType.findByName(params.contactType)
+			if(!contactType) {
+				log.warn "ContactType identified by id $params.contactType was not located"
+				render message(code: 'fedreg.contacttype.nonexistant', args: [params.contactID])
+				response.setStatus(500)
+				return
+			}
+
+			def contactPerson = new ContactPerson(contact:contact, type:contactType)
+
+
+
+			foreignFederation?.addToContacts(contactPerson)
+                        contactPerson.save()
+			if(contactPerson.hasErrors()) {
+				log.debug "$authenticatedUser failed to create $contactPerson linked to $contact for $foreignFederation"
+				contactPerson.errors.each {
+					log.error it
+				}
+				render message(code: 'fedreg.contactperson.create.error')
+				response.setStatus(500)
+				return
+			}
+
+			log.debug "$authenticatedUser created $contactPerson linked to $contact for $foreignFederation"
+			render message(code: 'fedreg.contactperson.create.success')
+		}
+		else {
+			log.warn("Attempt to link contact to $foreignFederation by $authenticatedUser was denied, incorrect permission set")
+			response.sendError(403)
+		}
+	}
+        def list = {
+		if(!params.id) {
+			log.warn "All name/value pairs required for this call were not present"
+			render message(code: 'fedreg.controllers.namevalue.missing')
+			response.setStatus(500)
+			return
+		}
+
+		def foreignFederation = ForeignFederation.get(params.id)
+		if(!foreignFederation) {
+			log.warn "RoleDescriptor identified by id $params.id was not located"
+			render message(code: 'fedreg.roledescriptor.nonexistant', args: [params.id])
+			response.sendError(500)
+			return
+		}
+
+		render template:"/templates/foreignFederationContacts/list", contextPath: pluginContextPath, model:[foreignFederation:foreignFederation]
+	}
+
+    	def delete = {
+		if(!params.id) {
+			log.warn "All name/value pairs required for this call were not present"
+			render message(code: 'fedreg.controllers.namevalue.missing')
+			response.setStatus(500)
+			return
+		}
+
+		def contactPerson = ContactPerson.get(params.id)
+		if(!contactPerson) {
+			log.warn "ContactPerson identified by id $params.id was not located"
+			render message(code: 'fedreg.contactperson.nonexistant', args: [params.id])
+			response.setStatus(500)
+			return
+		}
+                def foreignFederation = ForeignFederation.get(params.foreignFedId)
+		foreignFederation?.removeFromContacts(contactPerson)
+
+                contactPerson.save()
+		log.debug "$authenticatedUser deleted $contactPerson"
+		render message(code: 'fedreg.contactperson.delete.success')
+
+	
+}
+}
---
+++ foreignfederationoptin/grails-app/views/foreignFederation/edit.gsp
@@ -0,0 +1,116 @@
+
+<html>
+	<head>
+		<meta name="layout" content="members" />
+		<title><g:message code="fedreg.view.members.foreignFederation.edit.title" /></title>
+	</head>
+<body>
+		<r:script>
+			$(function() {
+				$('form').validate({
+						ignore: ":disabled",
+						success: function(label) {
+							if($(label).next())
+								$(label).next().remove()	// fix annoying bug where success labels are left laying about if duplicate validations
+							label.removeClass("error").addClass("icon icon_accept").html("&nbsp;");
+						},
+						keyup: false
+				});
+				$('form').formwizard({
+				 	formPluginEnabled: false,
+				 	validationEnabled: true,
+				 	focusFirstInput : true,
+					disableUIStyles: true
+				});
+			});
+		</r:script>
+
+	<section>
+	     <h2><g:message code="fedreg.view.members.foreignFederation.edit.heading" args="[foreignFederation.displayName]"/></h2>
+
+	     <g:form action="update" id="${foreignFederation.id}" method="PUT">
+	     <div class="step" id="basicinformation">
+		
+		<p>
+			<g:message code="fedreg.templates.foreignfederation.create.basicinformation.details" />
+		</p>
+
+		<table>
+			<tr>
+				<td>
+					<label for="foreignFederation.name"><g:message code="label.name" /></label>
+				</td>
+				<td>
+					<g:textField name="foreignFederation.name"  size="50" class="required" value="${foreignFederation?.name}"/>
+					<fr:tooltip code='fedreg.help.foreignfederation.name' />
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="foreignFederation.displayName"><g:message code="label.displayname" /></label>
+				</td>
+				<td>
+					<g:textField name="foreignFederation.displayName"  size="50" class="required" value="${foreignFederation?.displayName}"/>
+					<fr:tooltip code='fedreg.help.foreignfederation.displayName' />
+				</td>
+
+			</tr>
+                        <tr>
+                                 <td>
+					<label for="foreignFederation.description"><g:message code="label.description" /></label>
+				</td>
+                                <td>
+                                        <g:textArea name="foreignFederation.description"  class="required" rows="8" cols="36" value="${foreignFederation?.description}"/>
+					<fr:tooltip code='fedreg.help.foreignfederation.description' />
+                                </td>
+                        </tr>
+			<tr>
+				<td>
+					<label for="foreignFederation.websiteURL"><g:message code="label.foreignfederationurl" /></label>
+				</td>
+				<td>
+					<g:textField name="foreignFederation.websiteURL"  size="50" class="required url"  value="${foreignFederation?.websiteURL?.uri}"/>
+					<fr:tooltip code='fedreg.help.foreignfederation.url' />
+				</td>
+                       </tr>
+                        <tr>
+                                <td>
+					<label for="foreignFederation.metadataURL"><g:message code="label.metadataurl" /></label>
+				</td>
+				<td>
+					<g:textField name="foreignFederation.metadataURL"  size="50" class="required url"  value="${foreignFederation?.metadataURL?.uri}"/>
+					<fr:tooltip code='fedreg.help.foreignfederation.url' />
+				</td>
+                        </tr>
+                         <tr>
+                                <td>
+					<label for="foreignFederation.registrationInfo"><g:message code="label.registrationinfo" /></label>
+				</td>
+                                <td>
+					<g:textField name="foreignFederation.registrationInfo"  size="50" class="required url"  value="${foreignFederation?.registrationInfo?.uri}"/>
+					<fr:tooltip code='fedreg.help.foreignfederation.url' />
+				</td>
+
+			</tr>
+		</table>
+	   </div>
+
+	   <div class="step submit_step" id="creationsummary">
+		<h3><g:message code="fedreg.templates.foreignfederation.create.summary.heading" /></h3>
+		<p>
+			<g:message code="fedreg.templates.foreignfederation.create.summary.details" />
+                       
+		</p>
+          </div>
+
+	  <nav>
+	        <input id="back" value="${g.message(code:'label.back')}" type="reset" />
+		<input id="next" value="${g.message(code:'label.next')}" type="submit" />
+	  </nav>
+
+	  </g:form>
+	</section>
+</body>
+
+
+</html>
